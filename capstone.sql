0e7e65d9b83244e86e8ebf89f6a4b4c4b2179ef3
Avery Myart <amyart@amyart-ltm3ygh.internal.salesforce.com>
2023-07-18 10:30:23 -0700

    Created API & UI folders. Added sql and schema file

---
+++ capstone.sql
@@ -0,0 +1,8 @@
+\echo 'Delete and recreate capstone db?'
+\prompt 'Press return for yes or control-C for cancel > ' answer
+
+DROP DATABASE IF EXISTS capstone;
+CREATE DATABASE capstone;
+\connect capstone;
+
+\i capstone-schema.sql
