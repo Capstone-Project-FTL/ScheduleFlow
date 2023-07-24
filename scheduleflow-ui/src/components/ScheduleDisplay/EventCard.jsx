07366f06855f087fddffdb8728597b11ed8c6640
Chukwuemeka Ifejiagwa <cifejiagwa@salesforce.com>
2023-07-24 15:30:45 -0700

    scheduler ui works basic

--- scheduleflow-ui/src/components/ScheduleDisplay/EventCard.jsx
+++ scheduleflow-ui/src/components/ScheduleDisplay/EventCard.jsx
@@ -1,18 +1,37 @@
-export default function EventCard({start, end}) {
-  const gridStart = new Date()
-  gridStart.setHours(6, 30, 0)
-
+// 2.5 rem is used becuse thats the height of each cell block
+// I may abstact that away
+// Pixel offset: 1px based on the dividers thickness
+export default function EventCard({ start, end }) {
+  const gridStart = new Date();
+  gridStart.setHours(6, 30, 0);
   const getBlocksFromTop = (start) => {
     // console.log(start.getSeconds())
     // console.log(gridStart)
-    const startSec = start.getHours() * 3600 + start.getMinutes() * 60 + start.getSeconds()
-    const gridStartSec =  gridStart.getHours() * 3600 + gridStart.getMinutes() * 60 + gridStart.getSeconds()
-    return Math.abs(gridStartSec - startSec)/(60 * 15)
-  }
+    const startSec =
+      start.getHours() * 3600 + start.getMinutes() * 60 + start.getSeconds();
+    const gridStartSec =
+      gridStart.getHours() * 3600 +
+      gridStart.getMinutes() * 60 +
+      gridStart.getSeconds();
+    return Math.abs(gridStartSec - startSec) / (60 * 15);
+  };
+
+  const getHeight = (start, end) => {
+    const startSec =
+      start.getHours() * 3600 + start.getMinutes() * 60 + start.getSeconds();
+      const endSec =
+      end.getHours() * 3600 + end.getMinutes() * 60 + end.getSeconds();
+      return Math.abs(endSec - startSec)/(60 * 15)
+  };
 
   return (
-    <div className={"event-card rounded-md absolute bg-indigo-200 w-full h-64 border-none"} style={{top: `calc(2.5rem*${getBlocksFromTop(start)})`}}>
+    <div
+      className={
+        "event-card rounded-md absolute bg-indigo-200 w-full h-10 border-none"
+      }
+      style={{ top: `calc(2.5rem*${getBlocksFromTop(start)} + 1px)`, height: `calc(2.5rem * ${getHeight(start, end)} + 1px)`}}
+    >
       Event
     </div>
-  )
+  );
 }
--- scheduleflow-ui/src/components/ScheduleDisplay/ScheduleContentGrid.jsx
+++ scheduleflow-ui/src/components/ScheduleDisplay/ScheduleContentGrid.jsx
@@ -1,14 +1,15 @@
 import EventCard from "./EventCard";
 
 export default function ScheduleContentGrid() {
-  const start = new Date();
+  const start = new Date(1970, 0, 1);
   start.setHours(6, 15, 0);
-  const end = new Date();
+  const end = new Date(1970, 0, 1);
   end.setHours(22, 0, 0);
-  const mockStart = new Date();
-  mockStart.setHours(13, 0, 0);
-  const mockEnd = new Date();
-  mockEnd.setHours(9, 50, 0);
+  const times = [{startTime: new Date(1970, 0, 1, 7, 30, 0, 0), endTime: new Date(1970, 0, 1, 7, 40, 0, 0)},
+    {startTime: new Date(1970, 0, 1, 10, 30, 0, 0), endTime: new Date(1970, 0, 1, 12, 0, 0, 0)},
+    {startTime: new Date(1970, 0, 1, 20, 30, 0, 0), endTime: new Date(1970, 0, 1, 21, 40, 0, 0)},
+    {startTime: new Date(1970, 0, 1, 15, 30, 0, 0), endTime: new Date(1970, 0, 1, 16, 20, 0, 0)}
+  ]
   const numOfIterations = Math.ceil((end - start) / (3600 * 1000 * 0.25)); // 0.5 means 30 minutes per divider
   return (
     <div className="schedule-content-grid grid grid-cols-6 grid-flow-col-dense divide-x-2 divide-gray-300 text-xl text-black w-full ">
@@ -20,25 +21,26 @@ export default function ScheduleContentGrid() {
           .map((_, i) => {
             start.setTime(start.getTime() + 15 * 60 * 1000);
             return (
-                <p className="header-cell font-semibold px-4 flex justify-end items-center  text-neutral-500 h-10">
-                  {`${String(start.getHours()).padStart(2, "0")}:${String(
-                    start.getMinutes()
-                  ).padStart(2, "0")}`}
-                </p>
+              <p className="header-cell font-semibold px-4 flex justify-end items-center  text-neutral-500 h-10">
+                {`${String(start.getHours()).padStart(2, "0")}:${String(
+                  start.getMinutes()
+                ).padStart(2, "0")}`}
+              </p>
             );
           })}
       </div>
       {["Mon", "Tue", "Wed", "Thu", "Fri"].map((_, idx) => (
         <div
-          className={`time-span relative grid grid-flow-row divide-y-2 divide-gray-300 overflow-x-hidden`}
+          className={`time-span relative grid grid-flow-row divide-y-2 divide-gray-300 overflow-clip`}
           id={`${idx}`}
         >
           {Array(numOfIterations)
             .fill(0)
             .map((_, i) => (
-                <p className="h-10"></p>
+              <p className="h-10">
+                {times.map((e) => <EventCard start={e.startTime} end={e.endTime} />)}
+              </p>
             ))}
-            <EventCard start={mockStart} end={mockEnd} />
         </div>
       ))}
     </div>
