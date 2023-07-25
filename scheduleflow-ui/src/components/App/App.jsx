cbc4e39c82b06952c6be60405bf4579876d46432
Brian Barragan <bbarragan@bbarrag-ltmda6p.internal.salesforce.com>
2023-07-25 14:35:51 -0700

    Works on vite now

--- scheduleflow-ui/src/components/App/App.jsx
+++ scheduleflow-ui/src/components/App/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from "../LandingPage";
-import NavBar from "../Navbar";
-import Login from '../Login';
+import Landingpage from '../LandingPage/LandingPage';
+import NavBar from '../Navbar/Navbar';
+import Login from '../Login/Login';
+import HomePage from '../HomePage/HomePage';
+import Register from '../Register/Register';
+import ShoppingCart from '../CourseShoppingCart/CourseShoppingCart';
 import ScheduleDisplay from '../ScheduleDisplay/ScheduleDisplay';
 
 export default function App() {
@@ -10,15 +13,19 @@ export default function App() {
     document.title = "Course Flow";
   }, []);
   return (
+    <>
     <div className="App">
       <BrowserRouter>
-      {/* <NavBar/> */}
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<Landingpage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/schedule" element={<ScheduleDisplay />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/shoppingcart" element={<ShoppingCart/>}/>
         </Routes>
       </BrowserRouter>
     </div>
+    </>
   );
 }
