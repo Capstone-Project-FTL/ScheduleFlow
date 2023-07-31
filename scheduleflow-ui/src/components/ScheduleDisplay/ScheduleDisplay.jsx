bc22071f9ba4cdfe12202bcd046d24365bbee138
Chukwuemeka Ifejiagwa <cifejiagwa@salesforce.com>
2023-07-31 14:16:50 -0700

    merged the two implementations

--- scheduleflow-ui/src/components/ScheduleDisplay/ScheduleDisplay.jsx
+++ scheduleflow-ui/src/components/ScheduleDisplay/ScheduleDisplay.jsx
@@ -4,7 +4,7 @@ import ScheduleView from "./ScheduleView";
 
 export const CurrentScheduleContext = createContext();
 export default function ScheduleDisplay() {
-  const 
+  // const 
   return (
     <div className="schedule-display px-8 flex w-full h-screen items-center gap-x-12 bg-indigo-100">
       <CurrentScheduleContext.Provider>
