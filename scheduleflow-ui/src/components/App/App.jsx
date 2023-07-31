2fbc3b1e0b1e2209a1f14cefcd6f0957b59d9422
Chukwuemeka Ifejiagwa <cifejiagwa@salesforce.com>
2023-07-31 10:21:58 -0700

    fixed updated calendar view when user is taken to schedules route

--- scheduleflow-ui/src/components/App/App.jsx
+++ scheduleflow-ui/src/components/App/App.jsx
@@ -1,33 +1,40 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landingpage from '../LandingPage/LandingPage';
-import NavBar from '../Navbar/Navbar';
-import Login from '../Login/Login';
-import HomePage from '../HomePage/HomePage';
-import Register from '../Register/Register';
-import ShoppingCart from '../CourseShoppingCart/CourseShoppingCart';
-import ScheduleDisplay from '../ScheduleDisplay/ScheduleDisplay';
+import { useState, useEffect, createContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landingpage from "../LandingPage/LandingPage";
+import NavBar from "../Navbar/Navbar";
+import Login from "../Login/Login";
+import HomePage from "../HomePage/HomePage";
+import Register from "../Register/Register";
+import ShoppingCart from "../CourseShoppingCart/CourseShoppingCart";
+import ScheduleDisplay from "../ScheduleDisplay/ScheduleDisplay";
 
+export const AppStateContext = createContext();
 export default function App() {
   useEffect(() => {
     document.title = "Course Flow";
   }, []);
 
-  const [appState, setAppState] = useState({user: null, courses: null, schedules: null})
+  const [appState, setAppState] = useState({
+    user: null,
+    courses: null,
+    schedules: null,
+  });
   return (
     <>
-    <div className="App text-base">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/schedule" element={<ScheduleDisplay />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/shoppingcart" element={<ShoppingCart/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
+      <div className="App text-base">
+        <BrowserRouter>
+          <AppStateContext.Provider value={{appState, setAppState}}>
+            <Routes>
+              <Route path="/" element={<Landingpage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/schedule" element={<ScheduleDisplay />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/shoppingcart" element={<ShoppingCart />} />
+            </Routes>
+          </AppStateContext.Provider>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
