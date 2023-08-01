a3b2d530a82e31ff068e79df4cf4b67848577860
Avery Myart <amyart@amyart-ltm3ygh.internal.salesforce.com>
2023-08-01 13:40:51 -0700

    implemented package.json in root dir

--- capstone-api/config.js
+++ capstone-api/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 require('colors')
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3003
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
 
 
 function getDatabaseURI() {
