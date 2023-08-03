d2b0d75ef88bf0b1c76615766cf56698f4c7cc16
Avery Myart <amyart@amyart-ltm3ygh.internal.salesforce.com>
2023-08-03 13:20:22 -0700

    testing deploying db

--- scheduleflow-backend/capstone-api/hydrate_database.js
+++ scheduleflow-backend/capstone-api/hydrate_database.js
@@ -28,44 +28,44 @@ async function doesSectionExist(course_prefix, course_code, section_id) {
 async function hydrateDatabase() {
   try {
     // Asynchronously execute the SQL file that creates the database and tables
-    await pool.query(`
-      CREATE TABLE courses (
-        course_prefix VARCHAR(255) NOT NULL,
-        course_code VARCHAR(20) NOT NULL,
-        term VARCHAR(50),
-        course_description VARCHAR(255),
-        CONSTRAINT course_id PRIMARY KEY (course_prefix, course_code)
-      );
-    `);
-    await pool.query(`
-      CREATE TABLE sections (
-        section_id VARCHAR(50) NOT NULL,
-        course_prefix VARCHAR(255) NOT NULL,
-        course_code VARCHAR(20) NOT NULL,
-        instructors TEXT,
-        term VARCHAR(50),
-        section_days VARCHAR(50),
-        section_start_time VARCHAR(100),
-        section_end_time VARCHAR(100),
-        FOREIGN KEY (course_prefix, course_code) REFERENCES courses (course_prefix, course_code),
-        CONSTRAINT section_key PRIMARY KEY (course_prefix, course_code, section_id)
-      );
-    `);
-    await pool.query(`
-      CREATE TABLE labs (
-        lab_id VARCHAR(50) NOT NULL,
-        section_id VARCHAR(50) NOT NULL,
-        course_prefix VARCHAR(255) NOT NULL,
-        course_code VARCHAR(20) NOT NULL,
-        lab_name VARCHAR(50),
-        lab_instructors VARCHAR(100),
-        term VARCHAR(50),
-        lab_days VARCHAR(50),
-        lab_start_time VARCHAR(100),
-        lab_end_time VARCHAR(100),
-        FOREIGN KEY (course_prefix, course_code, section_id) REFERENCES sections (course_prefix, course_code, section_id)
-      );
-    `);
+    // await pool.query(`
+    //   CREATE TABLE courses (
+    //     course_prefix VARCHAR(255) NOT NULL,
+    //     course_code VARCHAR(20) NOT NULL,
+    //     term VARCHAR(50),
+    //     course_description VARCHAR(255),
+    //     CONSTRAINT course_id PRIMARY KEY (course_prefix, course_code)
+    //   );
+    // `);
+    // await pool.query(`
+    //   CREATE TABLE sections (
+    //     section_id VARCHAR(50) NOT NULL,
+    //     course_prefix VARCHAR(255) NOT NULL,
+    //     course_code VARCHAR(20) NOT NULL,
+    //     instructors TEXT,
+    //     term VARCHAR(50),
+    //     section_days VARCHAR(50),
+    //     section_start_time VARCHAR(100),
+    //     section_end_time VARCHAR(100),
+    //     FOREIGN KEY (course_prefix, course_code) REFERENCES courses (course_prefix, course_code),
+    //     CONSTRAINT section_key PRIMARY KEY (course_prefix, course_code, section_id)
+    //   );
+    // `);
+    // await pool.query(`
+    //   CREATE TABLE labs (
+    //     lab_id VARCHAR(50) NOT NULL,
+    //     section_id VARCHAR(50) NOT NULL,
+    //     course_prefix VARCHAR(255) NOT NULL,
+    //     course_code VARCHAR(20) NOT NULL,
+    //     lab_name VARCHAR(50),
+    //     lab_instructors VARCHAR(100),
+    //     term VARCHAR(50),
+    //     lab_days VARCHAR(50),
+    //     lab_start_time VARCHAR(100),
+    //     lab_end_time VARCHAR(100),
+    //     FOREIGN KEY (course_prefix, course_code, section_id) REFERENCES sections (course_prefix, course_code, section_id)
+    //   );
+    // `);
 
     for (const course of jsonData.courses) {
       const { course_prefix, course_code, title, credits, sections } = course;
