9ca1a117aea5057add23a4671f9a3a9ac74b6e04
Chukwuemeka Ifejiagwa <91810174+emeka-ifejiagwa@users.noreply.github.com>
2023-08-03 14:05:36 -0700

    ux (#62)

--- scheduleflow-ui/src/components/CourseShoppingCart/CourseShoppingCart.jsx
+++ scheduleflow-ui/src/components/CourseShoppingCart/CourseShoppingCart.jsx
@@ -180,7 +180,6 @@ export default function ShoppingCart() {
                     <Menu.Button
                       className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2  font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 h-full"
                       onClick={(e) => {
-                        document.querySelector("#prefix").click()
                         setSearchText({ prefix: "", code: "" })}}>
                       {courseInputs[index].course_prefix}
                       <ChevronDownIcon
